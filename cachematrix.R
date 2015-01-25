3d8a8e98f04e8de7c17b394277f420efe77e905d
ERufian <github@rufian.zilbermann.com>
2015-01-25 15:34:30 -0500

    Use spaces for indentation
    Convert tab indentation to space indentation (previously there was a mix
    of tabs and spaces)

--- cachematrix.R
+++ cachematrix.R
@@ -17,17 +17,17 @@
 ## Create a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-		inv <- NULL
-		set <- function(y) {
-				x <<- y
-				inv <<- NULL		
-		}
-		get <- function() x
-		setsolve <- function(solve) inv <<- solve
-		getsolve <- function() inv
-		list(set = set, get = get,
-				setsolve = setsolve,
-				getsolve = getsolve)
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL        
+        }
+        get <- function() x
+        setsolve <- function(solve) inv <<- solve
+        getsolve <- function() inv
+        list(set = set, get = get,
+                setsolve = setsolve,
+                getsolve = getsolve)
 
 }
 
@@ -40,8 +40,8 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getsolve()
         if(!is.null(inv)) {
-        		message("getting cached data")
-        		return(inv)
+                message("getting cached data")
+                return(inv)
         }
         data <- x$get()
         inv <- solve(data, ...)
